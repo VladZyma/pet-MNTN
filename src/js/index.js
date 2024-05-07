e50b8cb99468f5e4fbab07da8b774bd9bbca7c6a
Vladyslav Zyma <vladzima13@gmail.com>
2024-05-07 18:26:36 +0300

    add mobile navigation

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 'use strict';
 const scrollDownBtnEl = document.querySelector('.hero__btn');
+const headerEl = document.querySelector('.header');
+const headerNavEl = document.querySelector('.header__nav');
+const mobileNavBtnEl = document.querySelector('.mobile-btn');
 
 // SMOOTH SCROLLING
 scrollDownBtnEl.addEventListener('click', function (e) {
@@ -8,4 +11,19 @@ scrollDownBtnEl.addEventListener('click', function (e) {
   const sectionId = scrollDownBtnEl.getAttribute('href');
 
   document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
+});
+
+// MOBILE NAVIGATION
+headerEl.addEventListener('click', function (e) {
+  const targetEl = e.target;
+
+  if (
+    targetEl.className.includes('mobile-btn') ||
+    targetEl.className.includes('header__nav-link') ||
+    targetEl.className.includes('header__nav-account-link')
+  ) {
+    mobileNavBtnEl.classList.toggle('mobile-btn--active');
+    headerNavEl.classList.toggle('header__nav--active');
+    document.body.classList.toggle('_no-scroll');
+  }
 });
