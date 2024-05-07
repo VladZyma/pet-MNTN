700ef30667870c6144aef9f4eebfdf12ef65064a
Vladyslav Zyma <vladzima13@gmail.com>
2024-05-07 17:54:52 +0300

    add smooth scroll to section steps

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
+'use strict';
+const scrollDownBtnEl = document.querySelector('.hero__btn');
 
+// SMOOTH SCROLLING
+scrollDownBtnEl.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const sectionId = scrollDownBtnEl.getAttribute('href');
+
+  document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
+});
