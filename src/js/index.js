8989fc963a5a340822d7352f0eeaf39463f0805e
Vladyslav Zyma <vladzima13@gmail.com>
2024-05-08 07:50:09 +0300

    add lazy image loading

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ const scrollDownBtnEl = document.querySelector('.hero__btn');
 const headerEl = document.querySelector('.header');
 const headerNavEl = document.querySelector('.header__nav');
 const mobileNavBtnEl = document.querySelector('.mobile-btn');
+const imgTargets = document.querySelectorAll('.steps__img');
 
 // SMOOTH SCROLLING
 scrollDownBtnEl.addEventListener('click', function (e) {
@@ -27,3 +28,42 @@ headerEl.addEventListener('click', function (e) {
     document.body.classList.toggle('_no-scroll');
   }
 });
+
+// IMAGE LAZY LOADING
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.childNodes.forEach((target) => {
+    if (target.localName === 'source') {
+      target.srcset = target.dataset.srcset;
+    }
+
+    if (target.localName === 'img') {
+      const imgUrl = target.dataset.src.slice(
+        0,
+        target.dataset.src.lastIndexOf('.')
+      );
+
+      const imgFormat = target.dataset.src.slice(
+        target.dataset.src.lastIndexOf('.')
+      );
+
+      target.src = target.dataset.src;
+      target.srcset = `${imgUrl}@2x${imgFormat}`;
+
+      entry.target.classList.remove('_lazy-img');
+    }
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
